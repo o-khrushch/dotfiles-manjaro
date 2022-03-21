e9890f05d71b95a26438b4070f3e84706c94b0f3
Oleksii Khrushch <o.khrushch@outlook.com>
2022-03-21 23:12:45 +0200

    add themes and icons

--- packages/user/install.sh
+++ packages/user/install.sh
@@ -4,3 +4,17 @@ set -x
 
 sudo cp -r backgrounds/* /usr/share/backgrounds
 sudo cp -r avatars/* /usr/share/avatars
+
+mkdir tmp
+
+for f in themes/*.zip ; do
+  unzip "$f" -d tmp
+done
+sudo mv -fn tmp/* /usr/share/themes
+
+for f in icons/*.zip ; do
+  unzip "$f" -d tmp
+done
+sudo mv -fn tmp/* /usr/share/icons
+
+rm -rf tmp
--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@ sudo pacman -Syyu --noconfirm
 sudo pacman -S --noconfirm git base-devel yay
 
 # Install
-sh ./packages/install.sh
+sh ./packages/packages.sh
 
 mkdir -p ~/dev
 
