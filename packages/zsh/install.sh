2e699ff39036fdf22ed1314c591cb15e3f350200
Oleksii Khrushch <o.khrushch@outlook.com>
2022-03-21 22:33:22 +0200

    improve zsh scripts, dynamic profile

--- packages/zsh/install.sh
+++ packages/zsh/install.sh
@@ -8,4 +8,5 @@ yay -S --noconfirm --repo zsh
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
-cp {.zshrc,.p10k.zsh} ~
+sed "s/YOUR_PROFILE/$USER/g" .zshrc > ~/.zshrc
+cp .p10k.zsh ~
