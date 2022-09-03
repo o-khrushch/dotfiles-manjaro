30bc1cb4e2741ef13ee83cc41b181db82284ae70
Oleksii Khrushch <oleksiikhr@outlook.com>
2022-09-04 00:47:25 +0300

    add pfx certificate

--- packages/dotnet/install.sh
+++ packages/dotnet/install.sh
@@ -8,5 +8,6 @@ yay -S --needed --noconfirm --repo dotnet-runtime aspnet-targeting-pack aspnet-r
 dotnet dev-certs https --clean
 dotnet dev-certs https --trust
 sudo -E dotnet dev-certs https -ep /usr/local/share/ca-certificates/aspnet/https.crt --format PEM
+dotnet dev-certs https -ep ${HOME}/.aspnet/https/aspnetapp.pfx -p password --format PFX
 certutil -d sql:$HOME/.pki/nssdb -A -t "P,," -n localhost -i /usr/local/share/ca-certificates/aspnet/https.crt
 certutil -d sql:$HOME/.pki/nssdb -A -t "C,," -n localhost -i /usr/local/share/ca-certificates/aspnet/https.crt
