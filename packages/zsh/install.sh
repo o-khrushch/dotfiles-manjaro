f575db0357b72be996ea85e295ebf15148cbe26e
Oleksii Khrushch <oleksiikhr@outlook.com>
2023-01-05 01:14:53 +0200

    configure p10k

--- packages/zsh/install.sh
+++ packages/zsh/install.sh
@@ -12,3 +12,13 @@ fi
 if [ ! -d "$HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting" ]; then
   git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "$HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting"
 fi
+
+cp .p10k.zsh ~
+
+# p10k configure
+#alias l="ls -lHa"
+#alias o="xdg-open ."
+#alias c="xclip"
+#alias v="xclip -o"
+#alias day="redshift -x"
+#alias night="redshift -O 3700"
